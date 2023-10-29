aab72018c691d40e14fb61af6c00e8497d966bec
unknown <catcorbin0919@gmail.com>
2023-10-28 22:51:22 -0600

    added basic starting code in html and js

---
+++ script.js
@@ -0,0 +1,35 @@
+const apiKey = 'a19999ae3ca37292a4f34a9d91df9f77';
+
+
+function searchWeather() {
+    const city = document.getElementById('cityInput').value;
+//Function to fetch weather data for the city using API
+
+//Update the UI with the weather data
+}
+
+//Function to display current weather
+function displayCurrentWeather(weatherData) {
+    const currentWeatherDiv = document.getElementById('currentWeather');
+//Update the currentWeather with current weather
+}
+
+//Function to display 5-day forecast
+function displayForecast(weatherData) {
+    const forecastDiv = document.getElementById('forecast');
+// Update the forecast with the forecast
+}
+
+//Function to update search history
+function updateSearchHistory(city) {
+    const searchHistoryDiv = document.getElementById('searchHistory');
+//Update the searchHistory local storage with new city
+}
+
+//Function for when search history is clicked
+document.getElementById('searchHistory').addEventListener('click', function(event) {
+    if (event.target && event.target.nodeName == 'BUTTON') {
+        const city = event.target.textContent;
+        searchWeather(city);
+    }
+});
