f8247dc5293b96c65c0f6752a66109e7ccc1a17c
unknown <catcorbin0919@gmail.com>
2023-10-29 09:17:02 -0600

    designed ui

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-const apiKey ='https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a19999ae3ca37292a4f34a9d91df9f77}';
-
+const searchBtn = document.getElementById('searchBtn');
+const currentWeather = document.getElementById('currentWeather');
+searchBtn.addEventListener('click', searchWeather)
 function searchWeather() {
     const city = document.getElementById('cityInput').value;
-//Function to fetch weather data for the city using API
-
-//Update the UI with the weather data
+    //Function to fetch weather data for the city using API
+    displayCurrentWeather(city)
+    //Update the UI with the weather data
+    displayForecast(city)
 }
 
 //Function to display current weather
-function displayCurrentWeather(weatherData) {
-    const currentWeatherDiv = document.getElementById('currentWeather');
-//Update the currentWeather with current weather
+function displayCurrentWeather(city) {
+    const currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a19999ae3ca37292a4f34a9d91df9f77&units=imperial`;
+    let fetchRes = fetch(
+        currentUrl)
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+            currentWeather.innerHTML = `   <h2>
+            ${d.name} (10/29/2023)
+            <img src="https://openweathermap.org/img/wn/10d@2x.png">
+        </h2>
+        <p>Temp:</p>
+        <p>Wind:</p>
+        <p>Humidity:</p>`
+        })
+    //Update the currentWeather with current weather
 }
 
 //Function to display 5-day forecast
-function displayForecast(weatherData) {
-    const forecastDiv = document.getElementById('forecast');
-// Update the forecast with the forecast
+function displayForecast(city) {
+    const apiKey = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a19999ae3ca37292a4f34a9d91df9f77&units=imperial`;
+    let fetchRes = fetch(
+        apiKey)
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+        })
+    // Update the forecast with the forecast
 }
 
 //Function to update search history
 function updateSearchHistory(city) {
-    const searchHistoryDiv = document.getElementById('searchHistory');
-//Update the searchHistory local storage with new city
+    const searchHistory = document.getElementById('searchHistory');
+    //Update the searchHistory local storage with new city
 }
 
 //Function for when search history is clicked
-document.getElementById('searchHistory').addEventListener('click', function(event) {
+document.getElementById('searchHistory').addEventListener('click', function (event) {
     if (event.target && event.target.nodeName == 'BUTTON') {
         const city = event.target.textContent;
         searchWeather(city);
